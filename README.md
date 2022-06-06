f0c5d9c86e7e2047b98a29a9fb6c834180c0db84
Alex DeBrie <alexdebrie1@gmail.com>
2022-06-06 17:14:26 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,14 +1,44 @@
-# Welcome to your CDK TypeScript project
+## GraphQL + DynamoDB -- Single-table example
 
-This is a blank project for CDK development with TypeScript.
+This repository includes an example of building a GraphQL with DynamoDB using a single DynamoDB table. It is intended to pair with [this guide on using DynamoDB in a GraphQL API](TODO).
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+### Table of Contents
 
-## Useful commands
+- [Usage](#usage)
+- [Application background](#application-background)
+- [Takeaways](#takeaways)
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+## Usage
+
+This application uses the [AWS CDK](https://docs.aws.amazon.com/cdk/v2/guide/home.html) to deploy a GraphQL API to [AWS AppSync](https://aws.amazon.com/appsync/). Be sure to [install and bootstrap the CDK](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html#getting_started_install) before use.
+
+To deploy, clone this repository and run the following commands:
+
+```
+npm i
+cdk deploy
+```
+
+This will deploy an AppSync API and return the GraphQL root URL and an API key to access the API.
+
+You can query the API in the AWS console, or you can open the [`index.html`](./index.html) file to use a local GraphQL explorer. Before opening the file, be sure to replace `GRAPHQL_ROOT` and `API_KEY` with the received values from your deploy.
+
+## Application background
+
+This sample application builds a portion of a SaaS blog hosting platform. Users create a Site on the platform and are able to create Posts on the site. Other users can view the Posts and attach a Comment to a Post.
+
+The simplified ERD is below. This application includes the general CDK code and AppSync resolvers to demonstrate the key differences between single-table and multi-table design with GraphQL + DynamoDB.
+
+![AppSync - ERD](https://user-images.githubusercontent.com/6509926/172209448-98350f3f-7fcf-4a7e-aa64-123dd59ab4e9.svg)
+
+## Takeaways
+
+- **The DynamoDB data modeling uses common single-table design principles.** The DynamoDB table has a composite primary key using the generic `PK` and `SK` for the attribute names. Further, each item written to the table includes a `_TYPE` attribute to help distinguish the particular type.
+
+- **Single-table design optimizes for latency over simplicity.** In using a single-table design, you can fetch multiple types of entities in a single request, as shown in the getSite Query request and response templates.
+
+  Notice the request template does a lookahead to see if the incoming query is asking for the `posts` property on the `site` entity and adjusts its DynamoDB Query accordingly. Further, the response template needs to prepare a response from the result set containing both Site and Post items.
+
+- **The single-table model adds some constraints.** In this single-table model, both Site and Post items use the Site's `domain` as the partition key. This enables us to fetch both in a single request.
+
+  In our example repository showing the same application with a multi-table design, the Post items use a `siteId` as the partition key. Because Post items are retrieved after the Site is retrieved, it can use a more normalized model. This makes it easier to change the domain of Site and Post items in that example, at the cost of increased latency for common requests.
--- lib/single-table-cdk-stack.ts
+++ lib/single-table-cdk-stack.ts
@@ -1,16 +1,91 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AuthorizationType, FieldLogLevel, GraphqlApi, MappingTemplate, Schema } from "@aws-cdk/aws-appsync-alpha";
+import { CfnApiKey } from 'aws-cdk-lib/aws-appsync';
+import { CfnOutput } from 'aws-cdk-lib';
 
 export class SingleTableCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const dynamodbTable = new Table(this, 'DynamoDBTable', {
+      partitionKey: { name: 'PK', type: AttributeType.STRING},
+      sortKey: { name: 'SK', type: AttributeType.STRING},
+      billingMode: BillingMode.PAY_PER_REQUEST
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SingleTableCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    const api = new GraphqlApi(this, 'Api', {
+      name: 'DynamoDBSingleTable',
+      schema: Schema.fromAsset('lib/schema.graphql'),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.API_KEY
+        }
+      },
+      logConfig: {
+        fieldLogLevel: FieldLogLevel.ALL
+      },
+      xrayEnabled: true
+    })
+
+    const tableDatasource = api.addDynamoDbDataSource('DynamoDBTable', dynamodbTable)
+
+    tableDatasource.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'createSite',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createSite.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createSite.response.vtl'),
+    })
+
+    tableDatasource.createResolver({
+      typeName: 'Query',
+      fieldName: 'getSite',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Query.getSite.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Query.getSite.response.vtl'),
+    })
+
+    tableDatasource.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'createPost',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createPost.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createPost.response.vtl'),
+    })
+
+    tableDatasource.createResolver({
+      typeName: 'Query',
+      fieldName: 'getPostsForSite',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Query.getPostsForSite.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Query.getPostsForSite.response.vtl'),
+    })
+
+    tableDatasource.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'createComment',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createComment.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createComment.response.vtl'),
+    })
+
+    tableDatasource.createResolver({
+      typeName: 'Post',
+      fieldName: 'comments',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Post.comments.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Post.comments.response.vtl'),
+    })
+
+    const apiKey = new CfnApiKey(this, 'GraphQLApiKey', {
+      apiId: api.apiId
+    })
+
+    new CfnOutput(this, 'GraphQLURLOutput', {
+      value: api.graphqlUrl,
+      exportName: 'GraphQLURL'
+    })
+
+    new CfnOutput(this, 'GraphQLApiKeyOutput', {
+      value: apiKey.attrApiKey,
+      exportName: 'GraphQLApiKey'
+    })
   }
 }
